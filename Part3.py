96e929ebceabf09a279629bf9c5c85da839d54bf
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-19 09:39:11 -0300

    19/05/2021

---
+++ Part3.py
@@ -0,0 +1,8 @@
+# Variables and Datatypes
+character_name = "George"
+character_age = 40 # you just need quotes ("") when it is a string
+is_Male = True      
+print("There once was a man named", character_name + ",") # "+" separates so that print the variable text and the text withot adding spaces between
+print("he was",character_age,"years old.")                # "," separetes so that print the variable text and the text adding spaces between 
+print("He really liked the name " + character_name)
+print("but didn't like being " + character_age + ".")
---
+++ Part4.py
@@ -0,0 +1,10 @@
+# Working with strings
+phrase = ("Giraffe\n\"Academy\"") # "\n" is used to jump to a new line. '\"' is used to print quotes(")
+print(phrase.lower() + "is cool")
+print(phrase.upper() + "is cool")
+print(phrase.islower()) # it will print return (and print in this case) true or false if the text is all lower case (se o texto estiver todo em letra minúscula)
+print(phrase.upper().isupper()) # it will printe always true
+print (len(phrase)) # it will print the length of the string phrase
+print(phrase[0]) # it will print the first character (G), it begin counting at position 0
+print(phrase.index("Acad")) # it will print the position where the given parameter "Acad" starts inside of the string (9 in this case)
+print(phrase.index("z")) # it will print a error because the given parameter "z" does not exist in the string phrase
