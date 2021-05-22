ddc212b23a6042c23c1c056612b3acf027b0bfbe
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-22 04:05:46 -0300

    22/05/2021

--- Part4.py
+++ Part4.py
@@ -8,3 +8,4 @@ print (len(phrase)) # it will print the length of the string phrase
 print(phrase[0]) # it will print the first character (G), it begin counting at position 0
 print(phrase.index("Acad")) # it will print the position where the given parameter "Acad" starts inside of the string (9 in this case)
 print(phrase.index("z")) # it will print a error because the given parameter "z" does not exist in the string phrase
+print(phrase.replace("Giraffe", "Elephant")) #it will print the phrase changing the word "Giraffe" by "Elephant"
---
+++ Part5.py
@@ -0,0 +1,15 @@
+from math import *
+print(-2.0987)
+print(5+(1*2))
+print(10 % 3)
+my_num = -7
+print(my_num, "menos sete") # if it was typed withe "+" like: print(my_num + "sete") it would give error,
+# to use the function print for different kinds like number and strings we need to use ","
+print(abs(my_num)) # absolute value "abs()"
+print(pow(7, 2)) # power "pow()" | seven raised to the power of 2
+print(min(1,4)) # minimum value "min()"
+print(max(1,4)) # maximum value "max()"
+print(round(1.4)) # rounding the given value
+print(floor(1.4)) # get the lower nearst integer (1 in this case)
+print(ceil(1.4)) # get the bigger nearst integer (2 in this case)
+print(sqrt(1.4)) # get the square root of the given value
