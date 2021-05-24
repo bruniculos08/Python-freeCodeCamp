dea49b21e13f96958e3399d6c9c3c4a1db216039
Bruno Rafael dos Santos <bruniculos2014@gmail.com>
2021-05-23 23:15:42 -0300

    Update Part11.py

--- Part11.py
+++ Part11.py
@@ -1,5 +1,5 @@
 coordinates = (4, 5) # create a tuple, look "[]" create a list and "()" a tuple, both can store different elements. 
-# *Important: tuple is immutable, if it be tried there will appear an error messege like: "... 'tuple' object does not support item assignment" )
-print(coordinates[0]) # it will print the element in index 0 of the tuple
-coordinateslist = [(4,5), (8,11), (7,"Hello")]
-print(coordinateslist[1]) # it will print the element(in this case a tuple) in index 1 of the list
+# *Important: tuple is immutable, if it be tried there will appear an error messege like: "... 'tuple' object does not support item assignment").
+print(coordinates[0]) # it will print the element in index 0 of the tuple.
+coordinateslist = [(4,5), (8,11), (7,"Hello"), (True, False)]
+print(coordinateslist[1]) # it will print the element(in this case a tuple) in index 1 of the list.
